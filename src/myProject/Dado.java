6d85703de8b68639d791986dec0dce16119591d6
JohanCastro7 <130941263+JohanCastro7@users.noreply.github.com>
2023-05-07 21:01:58 -0500

    first commit (#2)
    Co-authored-by: Johan Castro <johan.castro@infodeclat.com>

--- src/myProject/Dado.java
+++ src/myProject/Dado.java
@@ -2,10 +2,49 @@ package myProject;
 import java.util.Random;
 public class Dado {
     private int cara;
+    private String imagen;
+    private String estado;
+
+    public Dado(String estadoInicial)
+    {
+        estado = estadoInicial;
+    }
 
     public int getCara() {
         Random aleatorio = new Random();
         cara = aleatorio.nextInt(6)+1;
+        this.asignarImagen();
         return cara;
+    }
+
+    public void asignarImagen()
+    {
+        switch (cara)
+        {
+            case 1:
+                imagen = "meeple.png";
+                break;
+
+            case 2:
+                imagen = "nave_espacial.png";
+                break;
+
+            case 3:
+                imagen = "super_herue.png";
+                break;
+
+            case 4:
+                imagen = "corazon.png";
+                break;
+
+            case 5:
+                imagen = "dragon.png";
+                break;
+
+            case 6:
+                imagen = "42.png";
+                break;
+
+        }
     }
 }
