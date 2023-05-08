c23d5bba69a394effa74a55841c1d199dbbc08a4
Johan Castro <johan.castro@infodeclat.com>
2023-05-07 20:54:18 -0500

    first commit

--- src/myProject/GUI.java
+++ src/myProject/GUI.java
@@ -1,7 +1,12 @@
 package myProject;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * This class is used for ...
@@ -9,18 +14,35 @@ import java.awt.*;
  * @version v.1.0.0 date:21/03/2023
  */
 public class GUI extends JFrame {
+    private Header headerProjec;
+    private JLabel[] dadosActivos = new JLabel[10];
+    private JLabel[] dadosInactivos = new JLabel[10];
+
+    private ImageIcon imagenDadoPorDefecto;
+
+    private BufferedImage img;
     private JPanel panelDadosActivos;
     private JPanel panelDadosInactivos;
     private JPanel panelDadosUtilizados;
     private JPanel panelMarcadorPuntaje;
 
-    public GUI() {
+    public GUI()
+    {
         // Configurar la ventana principal
+        headerProjec = new Header("Geets Out Master -- Game", Color.black);
+        this.add(headerProjec, BorderLayout.NORTH);
         setTitle("Geek Masters");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Crear los cuatro paneles y asignarles un título
+
+        //dado2 = new JLabel(imagenDado);
+        //dado1.setPreferredSize(new Dimension(70,70));
+        //dado2.setPreferredSize(new Dimension(70,70));
+        //dado1.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        //dado2.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+
+
         JPanel panel1 = new JPanel();
         panel1.setBorder(BorderFactory.createTitledBorder("Dados Activos"));
         JPanel panel2 = new JPanel();
@@ -31,9 +53,29 @@ public class GUI extends JFrame {
         panel4.setBorder(BorderFactory.createTitledBorder("Dados Utilizados"));
         JPanel panel5 = new JPanel();
 
+        // Dados por defecto.
+        // ACTIVOS
+        imagenDadoPorDefecto = new ImageIcon(new ImageIcon(getClass().getResource("/resources/caras/interrogante.png")).getImage().getScaledInstance(70,70, 1));
+        for (int i = 0; i < 7 ; i++)
+        {
+            dadosActivos[i] = new JLabel(imagenDadoPorDefecto);
+            dadosActivos[i].setPreferredSize(new Dimension(70,70));
+            dadosActivos[i].setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+            panel1.add(dadosActivos[i]);
+        }
+
+        //INACTIVOS
+        for (int i = 0; i < 3 ; i++)
+        {
+            dadosInactivos[i] = new JLabel(imagenDadoPorDefecto);
+            dadosInactivos[i].setPreferredSize(new Dimension(70,70));
+            dadosInactivos[i].setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+            panel2.add(dadosInactivos[i]);
+        }
 
         // Añadir componentes gráficos a cada panel
-        panel1.add(new JLabel("Panel 1"));
+        //panel1.add(dado1);
+        //panel1.add(dado2);
 
         // Añadir los cuatro paneles a la ventana principal
         JPanel panel_juego = new JPanel();
@@ -58,7 +100,10 @@ public class GUI extends JFrame {
         setVisible(true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+
         GUI juego = new GUI();
+        MoldelGeek model = new MoldelGeek();
     }
 }
--- src/myProject/MoldelGeek.java
+++ src/myProject/MoldelGeek.java
@@ -1,4 +1,32 @@
 package myProject;
 
 public class MoldelGeek {
+    private Dado[] dados;
+
+    private int tiro, punto, estado;
+    private String estadoString;
+    private int[] caras;
+
+    public MoldelGeek()
+    {
+        dados[0] = new Dado("activo");
+        dados[1] = new Dado("activo");
+        dados[2] = new Dado("activo");
+        dados[3] = new Dado("activo");
+        dados[4] = new Dado("activo");
+        dados[5] = new Dado("activo");
+        dados[6] = new Dado("activo");
+        dados[7] = new Dado("inactivo");
+        dados[8] = new Dado("inactivo");
+        dados[9] = new Dado("inactivo");
+    }
+
+    public void tiroInicial()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            caras[i] = dados[i].getCara();
+        }
+    }
+
 }
